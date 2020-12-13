e33cb6d4d757abd6272b12cd4bb40826039b6a3d
Ahmad <aalkhald@ford.com>
2020-12-13 15:47:44 -0500

    001args/002process

---
+++ readme.md
@@ -0,0 +1,9 @@
+# NodeJs Automation
+
+- This project will help on understanding how to automate tasks in NodeJs
+- It will go throw NodeJs core modules that can help us to that.
+
+## Chapters under /src
+
+- 001 Arguments.
+- 002 Process.
---
+++ src/002-process.js
@@ -0,0 +1,22 @@
+// When you run a script its treated as its won process.
+// This process has its own  PID
+// console.log(process.pid)
+
+// process.on('exit', (code) => {
+//   console.log(`The process has now finished  existing with code ${code}`)
+// });
+
+/*
+Stdin :
+  - User types something to terminal
+  - Then process data or chunks to be processed. 
+stdout :
+  - Is what returned back to the user.
+  - \n with process.stdout.write must be used.
+*/
+
+process.stdout.write('Hello I am writing to standard output\n');
+
+process.stdout.write(`current working dir is ${process.cwd()}\n`);
+
+console.log(`This script has been running for ${process.uptime()}\n`)
